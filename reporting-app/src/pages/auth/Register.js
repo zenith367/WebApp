281534b3b31cf596d2bb081391f358806067f1f0
zenith367 <rorisangthakholi8@gmail.com>
2025-10-11 03:02:30 +0200

    Update Register.js

--- reporting-app/src/pages/auth/Register.js
+++ reporting-app/src/pages/auth/Register.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://your-backend.onrender.com"; // 🔁 replace with your actual Render backend URL
+    : "https://backend-n6s1.onrender.com"// 🔁 replace with your actual Render backend URL
 
 export default function Register() {
   const [form, setForm] = useState({
